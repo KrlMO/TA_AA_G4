a5954686cdfab923b7edafa96dc0ae8902aba149
USER <USER@DESKTOP-8FTSH9L>
2024-04-17 00:44:43 -0500

    Creacion del repositorio con modificaciones en los inserts usando los procedimientos

---
+++ CampoySoftTechModel/src/pe/edu/pucp/campoysoft/tech/model/DebitoYape.java
@@ -0,0 +1,90 @@
+
+package pe.edu.pucp.campoysoft.tech.model;
+
+import java.util.Date;
+import pe.edu.pucp.campoysoft.onlinemarket.model.Atencion;
+
+public class DebitoYape {
+    private int id_debito_yape;
+    private String codigo_pago_yape;
+    //private blob
+    private String numero_cel;
+    private Date fecha_hora_emision;
+    private Date fecha_hora_pago;
+    private boolean activo;
+    private Atencion atencion;
+
+    public DebitoYape() {
+    }
+
+    public DebitoYape(int id_debito_yape, String codigo_pago_yape, String numero_cel, Date fecha_hora_emision, Date fecha_hora_pago, boolean activo, Atencion atencion) {
+        this.id_debito_yape = id_debito_yape;
+        this.codigo_pago_yape = codigo_pago_yape;
+        this.numero_cel = numero_cel;
+        this.fecha_hora_emision = fecha_hora_emision;
+        this.fecha_hora_pago = fecha_hora_pago;
+        this.activo = activo;
+        this.atencion = atencion;
+    }
+
+    
+
+    public int getId_debito_yape() {
+        return id_debito_yape;
+    }
+
+    public void setId_debito_yape(int id_debito_yape) {
+        this.id_debito_yape = id_debito_yape;
+    }
+
+    public String getCodigo_pago_yape() {
+        return codigo_pago_yape;
+    }
+
+    public void setCodigo_pago_yape(String codigo_pago_yape) {
+        this.codigo_pago_yape = codigo_pago_yape;
+    }
+
+    public String getNumero_cel() {
+        return numero_cel;
+    }
+
+    public void setNumero_cel(String numero_cel) {
+        this.numero_cel = numero_cel;
+    }
+
+    public Date getFecha_hora_emision() {
+        return fecha_hora_emision;
+    }
+
+    public void setFecha_hora_emision(Date fecha_hora_emision) {
+        this.fecha_hora_emision = fecha_hora_emision;
+    }
+
+    public Date getFecha_hora_pago() {
+        return fecha_hora_pago;
+    }
+
+    public void setFecha_hora_pago(Date fecha_hora_pago) {
+        this.fecha_hora_pago = fecha_hora_pago;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public Atencion getAtencion() {
+        return atencion;
+    }
+
+    public void setAtencion(Atencion atencion) {
+        this.atencion = atencion;
+    }
+    
+    
+    
+}
