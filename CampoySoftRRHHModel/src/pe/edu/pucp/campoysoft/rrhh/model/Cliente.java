d8aef3958a062242ded548cbdbff48c101a5ea9e
USER <USER@DESKTOP-8FTSH9L>
2024-04-30 20:00:31 -0500

    Modificiacion de Cliente.java

--- CampoySoftRRHHModel/src/pe/edu/pucp/campoysoft/rrhh/model/Cliente.java
+++ CampoySoftRRHHModel/src/pe/edu/pucp/campoysoft/rrhh/model/Cliente.java
@@ -37,10 +37,32 @@ public class Cliente extends Persona{
         this.activo = activo;
     }
 
+    public ArrayList<Atencion> getAtenciones() {
+        return atenciones;
+    }
+
+    public void setAtenciones(ArrayList<Atencion> atenciones) {
+        this.atenciones = atenciones;
+    }
+    
     @Override
     public String toString() {
         String str = super.toString();
         return "Cliente{" + "codCliente=" + codCliente + ", activo=" + activo + '}' + str;
     }
+    
+    public int eliminar_servicio_carrito(int id_servicio){ // Carlos
+        int resultado=0;
+        
+        for(int i=0;i<atenciones.size();i++){
+            if(atenciones.get(i).getIdAtencion()==id_servicio){
+                atenciones.remove(i);
+                resultado = 1;
+                return resultado;
+            }
+        }
+        return resultado;
+    }
+
    
 }
