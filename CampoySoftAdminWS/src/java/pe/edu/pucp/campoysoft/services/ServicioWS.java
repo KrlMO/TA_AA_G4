092a7e875da2a3b34a859e3a36bac785b4748d8e
sn1800 <a20190418@pucp.edu.pe>
2024-05-27 17:16:28 -0500

    1234

---
+++ CampoySoftAdminWS/src/java/pe/edu/pucp/campoysoft/services/ServicioWS.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package pe.edu.pucp.campoysoft.services;
+
+import jakarta.jws.WebService;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+import java.util.ArrayList;
+import pe.edu.pucp.campoysoft.rrhh.dao.EmpleadoDAO;
+import pe.edu.pucp.campoysoft.rrhh.model.Empleado;
+import pe.edu.pucp.campoysoft.rrhh.mysql.EmpleadoMySQL;
+
+/**
+ *
+ * @author FERNANDO
+ */
+@WebService(serviceName = "ServicioWS")
+public class ServicioWS {
+    
+    private EmpleadoDAO daoEmpleado;
+    
+    @WebMethod(operationName = "listarEmpleados")
+    public ArrayList<Empleado> listarEmpleados() {
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        try{
+            daoEmpleado = new EmpleadoMySQL();
+            empleados = daoEmpleado.listar();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+        return empleados;
+    }
+    @WebMethod(operationName = "modificarEmpleado")
+    public int modificarEmpleado(@WebParam(name = "empleado")Empleado empleado) {
+        int resultado = 0;
+        try{
+            daoEmpleado = new EmpleadoMySQL();
+            daoEmpleado.modificar(empleado);
+            resultado = 1;
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+}
