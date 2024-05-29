211df3ac70a7c3da29f466f3fc851d7c2c1466a8
CARLOS MONGE <calmori2011@hotmail.com>
2024-05-29 05:56:40 -0500

    ya porfa

--- CampoySoft/src/pe/edu/pucp/campoysoft/program/main/Principal.java
+++ CampoySoft/src/pe/edu/pucp/campoysoft/program/main/Principal.java
@@ -29,146 +29,123 @@ public class Principal {
         Empleado emp = new Empleado("EMP010" ,200, "Supervisor", true, "Sebastian", "Toledo", "Martinez", 72379187, new Date(), "Miraflores");
         EmpleadoDAO daoEmpleado = new EmpleadoMySQL();
         
-//        if(daoEmpleado.insertar(emp)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        ArrayList<ProductoRollo> productoRollos = new ArrayList<>();
-//        productoRollos = emp.listar_proStock_bajo();
-//        emp.reponer_stock(40, 100);
-//        for(int i=0;i<productoRollos.size();i++){
-//            String str= productoRollos.get(i).toString();
-//            System.out.println(str);
-//        }
-//          emp.despachar_Atencion(1);
+        if(daoEmpleado.insertar(emp)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+        ArrayList<ProductoRollo> productoRollos = new ArrayList<>();
+        productoRollos = emp.listar_proStock_bajo();
+        emp.reponer_stock(40, 100);
+        for(int i=0;i<productoRollos.size();i++){
+            String str= productoRollos.get(i).toString();
+            System.out.println(str);
+        }
+          emp.despachar_Atencion(1);
         ArrayList<Atencion> atenciones = new ArrayList<>();
         atenciones=emp.listar_Atencion_Emitidas();
         for(int i=0;i<atenciones.size();i++){
             String str= atenciones.get(i).toString();
             System.out.println(str);
         }
-//        emp = new Empleado("EMP002" ,200, "reponedor", true, "jesus", "cacerez", "Martinez", 72379187, new Date(), "Miraflores");
-//        if(daoEmpleado.insertar(emp)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        
-//        if(daoEmpleado.eliminar(1)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//
-//        Empleado emp2 = new Empleado();
-//        emp.setCargo("asistente");
-//        emp.setIdPersona(1);
-//        if(daoEmpleado.modificar(emp)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{  
-//            System.out.println("Ha ocurrido un error");
-//        }
-//
-//        ArrayList<Empleado> empleados = new ArrayList<>();
-//        empleados = daoEmpleado.listar();
-//        
-//        for(int i=0;i<empleados.size();i++){
-//            String str= empleados.get(i).toString();
-//            System.out.println(str);
-//        }
-//
-//        Cliente cli = new Cliente( "CLI001",true, "Marello", "Aguilar", "quispe",17302833,new Date(),"La victoria");
-//        ClienteDAO daoCliente = new ClienteMySQL();
-//        
-//        if(daoCliente.insertar(cli)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        cli = new Cliente( "CLI002",true, "Pepito", "Enrique", "toledo",17302833,new Date(),"La victoria");
-//        if(daoCliente.insertar(cli)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        if(daoCliente.eliminar(3)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        cli.setApPaterno("Diaz");
-//        cli.setIdPersona(4);
-//        if(daoCliente.modificar(cli)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{  
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        ArrayList<Cliente> clientes = new ArrayList<>();
-//        clientes = daoCliente.listar();
-//        
-//        for(int i=0;i<clientes.size();i++){
-//            String str= clientes.get(i).toString();
-//            System.out.println(str);
-//        }
-//        
-//
-//        
-//        Administrador adm = new Administrador( "ADM001",true, "Alvaro", "ventura", "santos",17302833,new Date(),"SJM");
-//        AdministradorDAO daoAdministrador = new AdministradorMySQL();
-//        if(daoAdministrador.insertar(adm)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        adm = new Administrador( "ADM002",true, "marlene", "quispe", "huaman",17302833,new Date(),"SJM");
-//        if(daoAdministrador.insertar(adm)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        
-//        if(daoAdministrador.eliminar(5)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        adm.setActivo(true);
-//        adm.setIdPersona(5);
-//        if(daoAdministrador.modificar(adm)==1){
-//            System.out.println("Se ha registrado con exito");
-//        }else{  
-//            System.out.println("Ha ocurrido un error");
-//        }
-//        ArrayList<Administrador> administradores = new ArrayList<>();
-//        administradores = daoAdministrador.listar();
-//        
-//        for(int i=0;i<administradores.size();i++){
-//            String str= administradores.get(i).toString();
-//            System.out.println(str);
-//        }
-//        
-//        
-//        
-//        Compra comp = new Compra(1003, 5, EstadoAtencion.Emitido, 10, 2000, 50, 300, emp, cli);
-//        CompraDAO daoCompra = new CompraMySQL();
-//        
-//        if(daoCompra.insertar(comp)==1)
-//            System.out.println("Se ha registrado la compra con exito");
-//        else
-//            System.out.println("Ha ocurrido un error");
-//        
-//        comp = new Compra(1004, 6, EstadoAtencion.Entregado, 20, 2500, 70, 120, emp, cli);
-//        
-//        if(daoCompra.insertar(comp)==1)
-//            System.out.println("Se ha registrado la compra con exito");
-//        else
-//            System.out.println("Ha ocurrido un error");
-//
-//        if(daoCompra.modificar(comp)==1){
-//            System.out.println("Se ha modificado con exito");
-//        }else{  
-//            System.out.println("Ha ocurrido un error");
-//        }
+        emp = new Empleado("EMP002" ,200, "reponedor", true, "jesus", "cacerez", "Martinez", 72379187, new Date(), "Miraflores");
+        if(daoEmpleado.insertar(emp)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+        
+        if(daoEmpleado.eliminar(1)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+
+        Empleado emp2 = new Empleado();
+        emp.setCargo("asistente");
+        emp.setIdPersona(1);
+        if(daoEmpleado.modificar(emp)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{  
+            System.out.println("Ha ocurrido un error");
+        }
+
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        empleados = daoEmpleado.listar();
+        
+        for(int i=0;i<empleados.size();i++){
+            String str= empleados.get(i).toString();
+            System.out.println(str);
+        }
+
+        Cliente cli = new Cliente( "CLI001",true, "Marello", "Aguilar", "quispe",17302833,new Date(),"La victoria");
+        ClienteDAO daoCliente = new ClienteMySQL();
+        
+        if(daoCliente.insertar(cli)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+        cli = new Cliente( "CLI002",true, "Pepito", "Enrique", "toledo",17302833,new Date(),"La victoria");
+        if(daoCliente.insertar(cli)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+        if(daoCliente.eliminar(3)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+        cli.setApPaterno("Diaz");
+        cli.setIdPersona(4);
+        if(daoCliente.modificar(cli)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{  
+            System.out.println("Ha ocurrido un error");
+        }
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        clientes = daoCliente.listar();
+        
+        for(int i=0;i<clientes.size();i++){
+            String str= clientes.get(i).toString();
+            System.out.println(str);
+        }
+        
+
+        
+        Administrador adm = new Administrador( "ADM001",true, "Alvaro", "ventura", "santos",17302833,new Date(),"SJM");
+        AdministradorDAO daoAdministrador = new AdministradorMySQL();
+        if(daoAdministrador.insertar(adm)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+        adm = new Administrador( "ADM002",true, "marlene", "quispe", "huaman",17302833,new Date(),"SJM");
+        if(daoAdministrador.insertar(adm)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+        
+        if(daoAdministrador.eliminar(5)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{
+            System.out.println("Ha ocurrido un error");
+        }
+        adm.setActivo(true);
+        adm.setIdPersona(5);
+        if(daoAdministrador.modificar(adm)==1){
+            System.out.println("Se ha registrado con exito");
+        }else{  
+            System.out.println("Ha ocurrido un error");
+        }
+        ArrayList<Administrador> administradores = new ArrayList<>();
+        administradores = daoAdministrador.listar();
+        
+        for(int i=0;i<administradores.size();i++){
+            String str= administradores.get(i).toString();
+            System.out.println(str);
+        }
     }
 }
