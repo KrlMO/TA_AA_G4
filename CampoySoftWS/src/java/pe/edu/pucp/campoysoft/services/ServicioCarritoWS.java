8d55e980d7dcc06d87642bc66de232363235cad1
sn1800 <a20190418@pucp.edu.pe>
2024-06-11 22:48:25 -0500

    1234

--- CampoySoftWS/src/java/pe/edu/pucp/campoysoft/services/ServicioCarritoWS.java
+++ CampoySoftWS/src/java/pe/edu/pucp/campoysoft/services/ServicioCarritoWS.java
@@ -7,9 +7,12 @@ package pe.edu.pucp.campoysoft.services;
 import jakarta.jws.WebService;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
+import java.util.ArrayList;
 import pe.edu.pucp.campoysoft.onlinemarket.dao.CompraDAO;
 import pe.edu.pucp.campoysoft.onlinemarket.dao.ServicioTinteDAO;
 import pe.edu.pucp.campoysoft.onlinemarket.model.Compra;
+import pe.edu.pucp.campoysoft.onlinemarket.model.LineaCompra;
+import pe.edu.pucp.campoysoft.onlinemarket.model.LineaServicioTinte;
 import pe.edu.pucp.campoysoft.onlinemarket.model.ServicioTinte;
 import pe.edu.pucp.campoysoft.onlinemarket.mysql.CompraMySQL;
 import pe.edu.pucp.campoysoft.onlinemarket.mysql.ServicioTinteMySQL;
@@ -20,16 +23,50 @@ import pe.edu.pucp.campoysoft.onlinemarket.mysql.ServicioTinteMySQL;
  */
 @WebService(serviceName = "ServicioCarritoWS")
 public class ServicioCarritoWS {
-
+    
+    private Compra compra;
+    private ServicioTinte servicio;
     private CompraDAO daoCompra;
     private ServicioTinteDAO daoServicio;
     
-    @WebMethod(operationName = "insertarCompra")
-    public int insertarCompra(@WebParam(name = "compra") Compra compra) {
+    @WebMethod(operationName = "insertarProductos")
+    public int insertarProductos(@WebParam(name = "productos") ArrayList<LineaCompra>productos ) {
         int resultado = 0;
         try{
-            daoCompra = new CompraMySQL();
-            resultado = daoCompra.insertar(compra);
+            if(compra ==null){
+                compra = new Compra();
+            }
+            compra.setLineaCompras(productos);
+            resultado = 1;
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    @WebMethod(operationName = "insertarServicios")
+    public int insertarServicios(@WebParam(name = "servicios") ArrayList<LineaServicioTinte>servicios ) {
+        int resultado = 0;
+        try{
+            if(servicio ==null){
+                servicio = new ServicioTinte();
+            }
+            servicio.setLineaServicios(servicios);
+            resultado = 1;
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return resultado;
+    }
+    
+    
+    @WebMethod(operationName = "insertarCompra")
+    public int insertarCompra() {
+        int resultado = 0;
+        try{
+            if(compra != null){
+                daoCompra = new CompraMySQL();
+                resultado = daoCompra.insertar(compra);
+            }   
         }catch(Exception ex){
             System.out.println(ex.getMessage());
         }
@@ -37,11 +74,13 @@ public class ServicioCarritoWS {
     }
     
     @WebMethod(operationName = "insertarServicio")
-    public int insertarServicio(@WebParam(name = "servicio") ServicioTinte servicio) {
+    public int insertarServicio() {
         int resultado = 0;
         try{
-            daoServicio = new ServicioTinteMySQL();
-            resultado = daoServicio.insertar(servicio);
+            if(servicio != null){
+                daoServicio = new ServicioTinteMySQL();
+                resultado = daoServicio.insertar(servicio);
+            }
         }catch(Exception ex){
             System.out.println(ex.getMessage());
         }
